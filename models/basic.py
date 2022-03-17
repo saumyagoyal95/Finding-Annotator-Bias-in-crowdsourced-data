d6a004454a65333aa8226b1418fcc12faab42b36
Saumya Goyal <saumyagoyal95@gmail.com>
2022-03-17 17:38:20 +0100

    Add files via upload

---
+++ models/basic.py
@@ -0,0 +1,50 @@
+from .utils import initialize_weight
+
+import torch.nn as nn
+import torch
+
+
+class BasicNetwork(nn.Module):
+    def __init__(self, embedding_dim, label_dim, use_softmax=True, apply_log=False):
+        super().__init__()
+
+        self.attention = nn.Linear(embedding_dim, 1, bias=False)
+        self.classifier = nn.Linear(embedding_dim, label_dim)
+        self.softmax_batch = nn.Softmax(dim=1)
+        self.softmax = nn.Softmax(dim=0)
+        self.sigmoid = nn.Sigmoid()
+
+        self.apply_log = apply_log
+        self.use_softmax = use_softmax
+
+        self.apply(initialize_weight)
+
+    def forward(self, x):
+
+        shape_len = len(x.shape)
+
+        # sum up word vectors weighted by their word-wise attentions
+        attentions = self.attention(x)
+        x = attentions * x
+
+        # sum over all words in x
+        if shape_len is 3:
+            x = x.sum(axis=1)
+        elif shape_len is 2:
+            x = x.sum(axis=0)
+
+        # feed it to the classifier
+        x = self.classifier(x)
+
+        if self.use_softmax:
+            if shape_len is 3:
+                x = self.softmax_batch(x)
+            elif shape_len is 2:
+                x = self.softmax(x)
+        else:
+            x = self.sigmoid(x)
+
+        if self.apply_log:
+            x = torch.clamp(torch.log(torch.clamp(x, 1e-5)), -100.0)
+
+        return x
---
+++ models/ipa2lt_head.py
@@ -0,0 +1,36 @@
+from .utils import initialize_weight, initialize_bias_matrices
+from .basic import BasicNetwork
+
+import torch.nn as nn
+import torch
+
+
+class Ipa2ltHead(nn.Module):
+    def __init__(self, embedding_dim, label_dim, annotator_dim, use_softmax=True, apply_log=False):
+        super().__init__()
+
+        self.annotator_dim = annotator_dim
+        self.label_dim = label_dim
+        self.apply_log = apply_log
+        self.basic_network = BasicNetwork(embedding_dim, label_dim, use_softmax=use_softmax)
+        self.bias_matrices = nn.ModuleList([nn.Linear(label_dim, label_dim, bias=False) for i in range(annotator_dim)])
+
+        self.basic_network.apply(initialize_weight)
+        self.bias_matrices.apply(initialize_bias_matrices)
+
+    def forward(self, x):
+
+        x = self.basic_network(x)
+        out = []
+        for matrix in self.bias_matrices:
+            # normalize rows of bias matrices
+            normalized = matrix.weight / torch.norm(matrix.weight, dim=1, p=1, keepdim=True)
+            matrix.weight = nn.Parameter(normalized.abs())
+
+            # forward pass, latent truth dimension is last dimension in x [batch_size, latent_truth]
+            pred = torch.matmul(x, matrix.weight)
+            if self.apply_log:
+                pred = torch.clamp(torch.log(torch.clamp(pred, 1e-5)), -100.0)
+            out.append(pred)
+
+        return out
